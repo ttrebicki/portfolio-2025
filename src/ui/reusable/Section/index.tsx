f3227ada4400109f5ae777bb72c9249e73da7f80
Tomasz Trębicki <tomek.trebicki@goingapp.pl>
2025-08-05 01:23:14 +0200

    fix(PO-3): fixes

--- src/ui/reusable/Section/index.tsx
+++ src/ui/reusable/Section/index.tsx
@@ -2,9 +2,7 @@ export const Section = (props: React.ComponentPropsWithoutRef<"section">) => {
   return (
     <section
       {...props}
-      className={`h-screen min-h-screen max-h-screen pt-16 pb-16 relative ${
-        props.className || ""
-      }`}
+      className={`pt-16 pb-16 relative ${props.className || ""}`}
     />
   );
 };
--- src/ui/sections/AboutMe/index.tsx
+++ src/ui/sections/AboutMe/index.tsx
@@ -12,7 +12,7 @@ export const AboutMe = () => {
           }
         >
           <Image
-            src={"/aboutme.jpeg"}
+            src={"/aboutme.png"}
             alt={"Tomasz Trębicki"}
             fill
             className={"flex-1"}
@@ -21,34 +21,34 @@ export const AboutMe = () => {
         </div>
         <div className={"flex flex-col gap-4 md:gap-8 flex-1"}>
           <Box>
-            <p className={"text-sm md:text-base"}>
+            <p>
               {
                 "With a foundation in cultural anthropology and UX research, I transitioned from design into full‑stack engineering at one of Poland’s top ticketing and event marketing firms. Over six years I’ve worn many hats—from CMS management and QA to UX/UI design and full‑stack development—ultimately specializing in TypeScript, Next.js, and React to deliver scalable, user‑centric applications."
               }
             </p>
           </Box>
-          <ul className={"flex flex-col gap-1 md:gap-4 text-xs md:text-base"}>
-            <li>
-              <p>
-                <strong>User‑First Thinking</strong>: I ground every feature in
-                real human needs.
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Clean, Maintainable Code</strong>: I adhere to best
-                practices and SOLID principles.
-              </p>
-            </li>
-            <li>
-              <p>
-                <strong>Continuous Improvement</strong>: I never stop learning
-                new tools and techniques.
-              </p>
-            </li>
-          </ul>
         </div>
       </div>
+      <ul className={"flex flex-col gap-1 md:gap-4 text-base md:text-2xl"}>
+        <li>
+          <p>
+            <strong>User‑First Thinking</strong>: I ground every feature in real
+            human needs.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Clean, Maintainable Code</strong>: I adhere to best
+            practices and SOLID principles.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Continuous Improvement</strong>: I never stop learning new
+            tools and techniques.
+          </p>
+        </li>
+      </ul>
     </div>
   );
 };
